feb54777982e3e45183149a540c0869977a2fac8
Simon Frankenberger <simon@fraho.eu>
2017-08-30 22:41:59 +0200

    Add examples for non-boot-starter dependencies

---
+++ regular-internal/src/main/java/eu/fraho/spring/example/RegularApplication.java
@@ -0,0 +1,36 @@
+/*
+ * MIT Licence
+ * Copyright (c) 2017 Simon Frankenberger
+ *
+ * Please see LICENCE.md for complete licence text.
+ */
+package eu.fraho.spring.example;
+
+import eu.fraho.spring.securityJwt.CryptPasswordEncoder;
+import eu.fraho.spring.securityJwt.config.CryptConfiguration;
+import lombok.extern.slf4j.Slf4j;
+import org.bouncycastle.jce.provider.BouncyCastleProvider;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+import java.io.IOException;
+import java.security.Security;
+
+@SpringBootApplication(scanBasePackages = {"eu.fraho.spring.example", "eu.fraho.spring.securityJwt"})
+@Slf4j
+public class RegularApplication {
+    static {
+        Security.addProvider(new BouncyCastleProvider());
+    }
+
+    public static void main(String[] args) throws IOException {
+        SpringApplication.run(RegularApplication.class, args);
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder(final CryptConfiguration configuration) {
+        return new CryptPasswordEncoder(configuration);
+    }
+}
