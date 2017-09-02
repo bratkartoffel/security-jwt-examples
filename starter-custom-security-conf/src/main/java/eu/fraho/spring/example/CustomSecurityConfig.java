4e822dcdd015d6bee91c91aaf9e3a8342defc48c
Simon Frankenberger <simon@fraho.eu>
2017-09-02 16:23:28 +0200

    Make custum security-config more verbose and easier to verify it's working

--- starter-custom-security-conf/src/main/java/eu/fraho/spring/example/CustomSecurityConfig.java
+++ starter-custom-security-conf/src/main/java/eu/fraho/spring/example/CustomSecurityConfig.java
@@ -9,13 +9,17 @@ package eu.fraho.spring.example;
 import eu.fraho.spring.securityJwt.JwtAuthenticationEntryPoint;
 import eu.fraho.spring.securityJwt.config.WebSecurityConfig;
 import eu.fraho.spring.securityJwt.service.JwtTokenService;
-import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.annotation.Order;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import javax.servlet.*;
+import java.io.IOException;
 
 @Configuration
 @Slf4j
@@ -34,8 +38,29 @@ public class CustomSecurityConfig extends WebSecurityConfig {
 
         log.info("Overriding ant matchers");
         httpSecurity
+                .addFilterBefore(customFilter(), UsernamePasswordAuthenticationFilter.class)
                 .authorizeRequests()
-                .antMatchers("/public/**").permitAll()
                 .anyRequest().authenticated();
+    }
+
+    @Bean
+    public Filter customFilter() {
+        return new Filter() {
+            @Override
+            public void init(FilterConfig filterConfig) throws ServletException {
+                log.info("Initializing");
+            }
+
+            @Override
+            public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+                log.info("Handling request");
+                chain.doFilter(request, response);
+            }
+
+            @Override
+            public void destroy() {
+                log.info("Destroying");
+            }
+        };
     }
 }
