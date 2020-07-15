3f8005facf0b2fe9195553b7b82f973b3b87b8c5
Simon Frankenberger <simon@fraho.eu>
2020-07-15 23:06:02 +0200

    upgrade to latest security-jwt release

--- starter-swagger/src/main/java/eu/fraho/spring/example/starter_swagger/PrivateController.java
+++ starter-swagger/src/main/java/eu/fraho/spring/example/starter_swagger/PrivateController.java
@@ -6,6 +6,8 @@
  */
 package eu.fraho.spring.example.starter_swagger;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import org.springframework.security.access.annotation.Secured;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,6 +15,10 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @Secured("ROLE_USER")
 public class PrivateController {
+    @Operation(summary = "Sample endpoint",
+            description = "This operation is protected by an api key",
+            security = {@SecurityRequirement(name = "apiKey")}
+    )
     @RequestMapping("/private")
     public String index() {
         return "Hello world!";
--- starter-swagger/src/main/java/eu/fraho/spring/example/starter_swagger/PublicController.java
+++ starter-swagger/src/main/java/eu/fraho/spring/example/starter_swagger/PublicController.java
@@ -6,11 +6,14 @@
  */
 package eu.fraho.spring.example.starter_swagger;
 
+import io.swagger.v3.oas.annotations.Operation;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class PublicController {
+    @Operation(summary = "Sample endpoint",
+            description = "This operation is publicly available, no key required")
     @RequestMapping("/public")
     public String hello() {
         return "Public area";
