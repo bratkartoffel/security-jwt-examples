8ef0bbf5157aa775c8c023288e211885163a2c8a
Simon Frankenberger <simon@fraho.eu>
2022-10-21 07:46:47 +0200

    fix tests with jakarta api instead of javax

---
+++ starter-cookies-jakarta/src/main/java/eu/fraho/spring/example/starter_cookies/CookiesApplication.java
@@ -0,0 +1,17 @@
+/*
+ * MIT Licence
+ * Copyright (c) 2020 Simon Frankenberger
+ *
+ * Please see LICENCE.md for complete licence text.
+ */
+package eu.fraho.spring.example.starter_cookies;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CookiesApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(CookiesApplication.class, args);
+    }
+}
